403e7f9f84cb93be3e9bc1998e69ca3597d360b0
Damian Budelewski <damian.budelewski@gmail.com>
2021-12-23 21:48:08 +0100

    day5 - feat(cs61a): add assert and docstring tests to piglatin func

--- cs61a/01_pigLatin.py
+++ cs61a/01_pigLatin.py
@@ -4,9 +4,23 @@ def pig_latin(sentence):
     Transfer initial consonant or consonant cluster of each word to the end
     of the word and adding a vocalic syllable (usually /eɪ/)
 
-    example: "pig latin" would be "igpay atinlay"
-    """
+    Test using docstring:
+    >>> pig_latin("pig latin")
+    'igpay atinlay'
 
+    How to test:
+    a) Using Python interpreter
+        .>>> from doctest import run_docstring_examples
+        .>>> run_docstring_examples(pig_latin, globals(), True)
+        Finding tests in NoName
+        Trying:
+            pig_latin("pig latin")
+        Expecting:
+            'igpay atinlay'
+        ok
+    b) Using CLI
+        python3 -m doctest 01_pigLatin.py
+    """
     try:
         transformed_sentence = str()
         for word in sentence.split(" "):
@@ -15,3 +29,10 @@ def pig_latin(sentence):
         return transformed_sentence[0:-1]
     except IndexError:
         print("Can't process empty strings ... try again with a valid sentence!")
+
+
+def pig_latin_test():
+    """Test pig latin translation on given sentence using assertion."""
+    assert (
+        pig_latin("pig latin") == "igpay atinlay"
+    ), "Wrong translation for 'pig latin' sentence"
