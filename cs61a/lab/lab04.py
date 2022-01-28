9e4e1d3fbdd6ae46d9b7f1d0d5d90a66c5477f90
Damian Budelewski <damian.budelewski@gmail.com>
2022-01-28 18:34:10 +0100

    day14 - feat(cs61a): add lab4 `summation` function

--- cs61a/lab/lab04.py
+++ cs61a/lab/lab04.py
@@ -19,7 +19,10 @@ def summation(n, term):
     True
     """
     assert n >= 1
-    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return term(n)
+    else:
+        return term(n) + summation(n - 1, term)
 
 
 def pascal(row, column):
